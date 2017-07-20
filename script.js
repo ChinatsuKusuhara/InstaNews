8aa87a855d47c5b1143b98b6226e71f7cd7efb33
Chinatsu <chinatsu.k@hotmail.com>
2017-07-20 00:12:07 -0700

    gulp and gitifnore files created

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $('button').on('click', function() {
    $.getJSON(
-      'https://api.github.com/users/octocat/repos?client_id=a37c6077034750f953fc&client_secret=8ff75658b21aa8c5830b7efeae85f559b4d36a02' )
+      '' )
     .done(function(data) {
     console.log(data);
       $.each(data, function( key, repos){
