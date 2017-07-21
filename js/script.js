1d217debd1182fb8f84e977cc0afa6aa0c431d61
Chinatsu <chinatsu.k@hotmail.com>
2017-07-21 16:43:18 -0700

    review psuedo code

--- js/script.js
+++ js/script.js
@@ -3,17 +3,21 @@ $(document).ready(function(){
     event.preventDefault();
     $.ajax({
       method= 'GET',
-      dataType= 'jsonp',
       url= 'https://api.nytimes.com/svc/topstories/v2/home.json'+$(".drop-down").val().replace(" ", "+")
     }) //end of .ajax
     
     .done (function(data){
       console.log(data);
       $.each(data.results, function(key, value){
-        
-      })
+        $('.drop-down').append('<selection> <option src="' + value.news + '">' + value.collectionName + '</selection>');
+      })//end of .each
     }) //end of .done
 
+    .fail(function(){
+      console.log('anything');
+     $(".drop-down").append("<li>No Results</li>");
+    }) //end of .fail
   }) //end of .on
+
 });  //end of doc.ready
 
