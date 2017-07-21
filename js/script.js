231346500f737a6dd162bbd2798de0f35cf07070
Chinatsu <chinatsu.k@hotmail.com>
2017-07-21 00:28:12 -0700

    keep working on .done js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,12 @@
 $(document).ready(function(){
+  $('.drop-down').on('change', function(event){
+    event.preventDefault();
+    $.ajax({
+      method= 'GET',
+      dataType= 'jsonp',
+      url= 'https://api.nytimes.com/svc/topstories/v2/home.json'+$(".drop-down").val().replace(" ", "+")
+    }) //end of .ajax
 
-
-  var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
-      url += '?' + $.param({
-        'api-key': '3924f5cbfd774746a80d6b85a0aa79a0'
-      });
-
-  $.ajax({
-    url: url,
-    method: 'GET',
-  }).done(function(result) {
-    console.log(result);
-    $('')
-    
-    $('.news').append(data);
-    console.log(data);
-
-  }).fail(function(err) {
-    throw err;
-  });
-
-
+  }); //end of .on
 });  //end of doc.ready
 
