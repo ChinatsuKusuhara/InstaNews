63695cc55ff473ffd44cb338a37b6ea53d7d1ba0
Chinatsu <chinatsu.k@hotmail.com>
2017-07-23 23:25:23 -0700

    make images show up.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('.drop-down').on('change', function(){
     
     var selected = $('.drop-down').val();
-    var  url = 'https://api.nytimes.com/svc/topstories/v2'+ selected + '.json';  //translation from computer language to English
+    var  url = 'https://api.nytimes.com/svc/topstories/v2/'+ selected + '.json';  //translation from computer language to English
          url += '?' + $.param({
           'api-key': '3924f5cbfd774746a80d6b85a0aa79a0'
     })     //'+=' assigns the result of the var, '?' connects api keys to url, .param = parameter
