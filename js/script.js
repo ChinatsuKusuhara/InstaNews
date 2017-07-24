3cdcd729ba4630c1f76ffdedb4c8bb3a1937d6c1
Chinatsu <chinatsu.k@hotmail.com>
2017-07-23 23:15:07 -0700

    fix error $.ajax

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-  $('.drop-down').on('change', function(event){
-    event.preventDefault();
+  $('.drop-down').on('change', function(){
     
     var selected = $('.drop-down').val();
     var  url = 'https://api.nytimes.com/svc/topstories/v2'+ selected + '.json';  //translation from computer language to English
@@ -11,6 +10,29 @@ $(document).ready(function(){
       method: 'GET',
       url: url
     }) //end of .ajax 
+    .done (function(data){
+      var article = '';
+      var dataResults = data.results.filter(function(value){
+            return value.multimedia.length >= 5;
+          })  //.filter reduces the set of matched elements 
+          dataResults.splice(12);  // .splice reduce the set of matched elements to a subset specified by a range of indices.
 
+      $.each(dataResults, function(key, value){
+        var picture = value.multimedia[4].url, 
+            abstract = value.abstract,
+            articleUrl = value.url; //will take client to a linked site 
+
+        article += '<li>';
+        article += "<a href=' + articleUrl + '>" ;  //extracting news articles
+        article += "<div class='background' style='background-image:url("; //giving a class to the background
+        article += picture; //images will become a background
+        article += ") '><p class='abstract'>";  //created a class called abstract
+        article += abstract;  
+        article += "</p></div></a></li>";
+      });//end of .each
+  
+      $(".news").html(article)//to load within <ul>
+      
+     }) //end of .done
   })  //end of .on
 }); //end of doc.ready
